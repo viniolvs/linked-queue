65e154a889abfc119154474a03e9a5e52f0fd355
Vinicius <vinioliveira.s@hotmail.com>
2022-02-27 19:56:02 -0300

    Adding model to info struct

--- fila_encadeada.h
+++ fila_encadeada.h
@@ -1,3 +1,6 @@
+#ifndef TINFO_T
+#define TINFO_T 1
+//tipo Info, armazena as informações contidas em cada elemento da fila
 typedef struct tInfo
 {
 	char nome_solicitante[31];
@@ -6,32 +9,49 @@ typedef struct tInfo
     char *data_entrega;
     double valor_conserto;
 }tInfo;
+#endif
 
+#ifndef TELEMENT_T
+#define TELEMENT_T 1
+//tipo elemento, representa os nós da fila encadeada
 typedef struct tElement
 {
 	tInfo *info;
 	struct tElement *next;
 }tElement;
+#endif
 
+#ifndef FILA_T
+#define FILA_T 1
+//Tipo fila
 typedef struct Fila
 {
 	tElement *head;
     tElement *tail;
 	int tamanho;
 }Fila;
+#endif
 
+//inicializa uma fila
 void newFila(Fila *fila);
 
+//verifica se uma fila está vazia
 short filaVazia (Fila *fila);
 
+//adiciona um novo  elemento no fim de uma fila
 int adicionaFim (Fila *fila, tInfo info);
 
+//retira e retorna o primeiro elemento da fila
 tInfo* retiraInicio (Fila *fila);
 
+//remove todos os elementos de uma fila
 void limpaFila(Fila *fila);
 
+//escreve no terminal os dados do tipo tInfo
 void printInfo(tInfo *info,int posicao);
 
+//escreve todos os elementos da fila
 void printFila (Fila fila);
 
+//pede ao usuário os dados de um novo concerto e insere-o no fim da fila
 void insereConcerto(Fila *fila);
